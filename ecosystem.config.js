970c07e290a21e0d4be78768c5a9b55ed5adde9c
Shadofer <shadowrlrs@gmail.com>
2021-10-11 13:50:19 +0300

    Fix node args, add custom instances param for  heroku.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,12 @@ module.exports = {
         //customise to your liking
         script: 'server.js',
         watch: ['server.js'],
-        watch_delay: 2000,
-        instances: 'max',
+        instances: process.env.FRONVO_PM2_INSTANCES || '1',
         exec_mode: 'cluster',
         max_memory_restart: process.env.FRONVO_PM2_MEM || '1G',
         autorestart: true,
         max_restarts: 5,
         env: {"TARGET_PM2": true},
-        args: "-nouse-idle-notification --max-old-space-size=8192 --max-new-space-size=2048"
+        node_args: "--nouse-idle-notification --max-old-space-size=8192 --max-new-space-size=2048"
     }]
 }
