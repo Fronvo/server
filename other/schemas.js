501c605a0fc439d1fd4d9d4bc548deb67d91de25
Shadofer <shadowrlrs@gmail.com>
2022-02-27 15:50:18 +0200

    Adjust regex.

--- other/schemas.js
+++ other/schemas.js
@@ -9,11 +9,11 @@ module.exports = {
     // Register / Login
     accountSchema: Joi.object({
         email: Joi.string().email().min(8).max(60).required(),
-        password: Joi.string().regex(new RegExp('[a-zA-Z0-9]')).min(8).max(30).required()
+        password: Joi.string().regex(/[a-zA-Z0-9]/).min(8).max(30).required()
     }),
 
     // Login with token
     accountTokenSchema: Joi.object({
-        token: Joi.string().length(36).regex(new RegExp('[a-z0-9-]')).required()
+        token: Joi.string().length(36).regex(/[a-z0-9-]/).required()
     })
 }
