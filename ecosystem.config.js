58dcf036fdb99a78d25877cf88d6188ec6d18b3e
Shadofer <shadowrlrs@gmail.com>
2022-03-10 16:42:16 +0200

    Adjust PM2 configuration.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         // Customise to your liking if you have to (you shouldn't most likely)
 
-        script: 'main/server.js',
+        script: 'output/main/server.js',
         instances: process.env.FRONVO_PM2_INSTANCES || '1',
         exec_mode: 'cluster',
         max_memory_restart: process.env.FRONVO_PM2_MEM || '1G',
