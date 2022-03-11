d565c761526468e5bc3cd935dc3bfcfefe75733f
Shadofer <shadowrlrs@gmail.com>
2022-03-11 14:03:28 +0200

    Maximize test efficacy.

--- test/main.js
+++ test/main.js
@@ -17,21 +17,21 @@ const { assert } = require('chai');
 const { io } = require('socket.io-client');
 const shared = require('./shared');
 
+// Create the client
+socket = io(`ws://localhost:${PORT}`, {
+	transports: ['websocket'],
+	path: '/fronvo',
+	reconnectionDelay: 100,
+	reconnectionDelayMax: 200,
+	reconnectionAttempts: 10,
+	randomizationFactor: 0
+});
+
 describe('Fronvo', () => {
 	let socket;
 	
 	// Before the tests start
 	before((done) => {
-		// Create the client
-		socket = io(`ws://localhost:${PORT}`, {
-			transports: ['websocket'],
-			path: '/fronvo',
-			reconnectionDelay: 100,
-			reconnectionDelayMax: 200,
-			reconnectionAttempts: 10,
-			randomizationFactor: 0
-		});
-
 		// Wait for the server to come alive
 		socket.on('connect', done);
 	});
