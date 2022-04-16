803b0a41e69e5168db11725a77420a5eb56749ba
Shadofer <shadowrlrs@gmail.com>
2022-04-16 21:53:25 +0300

    Format file.

--- src/utilities/all.ts
+++ src/utilities/all.ts
@@ -7,4 +7,9 @@ import * as utilitiesNoAccount from 'utilities/noAccount';
 import * as utilitiesGeneral from 'utilities/general';
 import * as utilitiesAccount from 'utilities/account';
 
-export default {...utilitiesAll, ...utilitiesNoAccount, ...utilitiesGeneral, utilitiesAccount};
+export default {
+    ...utilitiesAll,
+    ...utilitiesNoAccount,
+    ...utilitiesGeneral,
+    ...utilitiesAccount
+};
