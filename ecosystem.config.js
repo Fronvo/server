28906a885bbdedec2601251e50bfc465f9179773
Shadofer <shadowrlrs@gmail.com>
2022-04-18 18:36:55 +0300

    Remove PM2 max memory parameter.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,6 @@ module.exports = {
             script: 'output/main/server.js',
             instances: process.env.FRONVO_PM2_INSTANCES || '2',
             exec_mode: 'cluster',
-            max_memory_restart: process.env.FRONVO_PM2_MEM || '150M',
             autorestart: true,
             max_restarts: 5,
             env: {"FRONVO_TARGET_PM2": true},
