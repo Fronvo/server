88072fa013d60909bdcb6de18e7b2063269bda4e
Shadofer <shadowrlrs@gmail.com>
2022-04-27 19:47:26 +0300

    Whoops, update gradient version text.

--- src/main/server.ts
+++ src/main/server.ts
@@ -187,7 +187,7 @@ async function startup(): Promise<void> {
     preStartupChecks();
 
     // Gradient shenanigans
-    console.log(gradient(['#e8128f', '#e812d2', '#e412e8', '#cb12e8', '#bd12e8', '#a812e8', '#8f12e8', '#8012e8'])('Fronvo server v0.1'));
+    console.log(gradient(['#e8128f', '#e812d2', '#e412e8', '#cb12e8', '#bd12e8', '#a812e8', '#8f12e8', '#8012e8'])('Fronvo server v0.2'));
 
     // Special check because ora doesnt care
     if(!variables.silentLogging) {
